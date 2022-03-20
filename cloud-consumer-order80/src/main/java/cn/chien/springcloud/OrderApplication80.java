b4e8406c7dbb940c572f18f82abde037cad2b1d3
Giovanni <1300687449@qq.com>
2022-03-20 22:08:58 +0800

    register order and payment module into eureka

--- cloud-consumer-order80/src/main/java/cn/chien/springcloud/OrderApplication80.java
+++ cloud-consumer-order80/src/main/java/cn/chien/springcloud/OrderApplication80.java
@@ -2,12 +2,14 @@ package cn.chien.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author qian.diqi
  * @date 2022/3/20
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderApplication80 {
 
     public static void main(String[] args) {
--- cloud-provider-payment8001/src/main/java/cn/chien/springcloud/PaymentApplication8001.java
+++ cloud-provider-payment8001/src/main/java/cn/chien/springcloud/PaymentApplication8001.java
@@ -2,12 +2,14 @@ package cn.chien.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author qian.diqi
  * @date 2022/3/19
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentApplication8001 {
 
     public static void main(String[] args) {
