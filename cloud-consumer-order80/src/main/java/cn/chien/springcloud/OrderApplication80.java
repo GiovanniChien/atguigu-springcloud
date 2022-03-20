b790e085d4dd0aa636ab451d6e26f7c043a5a273
Giovanni <1300687449@qq.com>
2022-03-20 20:08:19 +0800

    extract common module and new order module

---
+++ cloud-consumer-order80/src/main/java/cn/chien/springcloud/OrderApplication80.java
@@ -0,0 +1,17 @@
+package cn.chien.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author qian.diqi
+ * @date 2022/3/20
+ */
+@SpringBootApplication
+public class OrderApplication80 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication80.class, args);
+    }
+
+}
---
+++ cloud-consumer-order80/src/main/java/cn/chien/springcloud/controller/OrderController.java
@@ -0,0 +1,18 @@
+package cn.chien.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author qian.diqi
+ * @date 2022/3/20
+ */
+@RestController
+@Slf4j
+@RequestMapping("order")
+public class OrderController {
+
+
+
+}
